b94c5732cfd1c239d7efe4135d8fc2a60f533bc6
developer <khanabdur132@gmail.com>
2026-05-13 09:59:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ function loadQuestions(lang) {
     const today = 'day' + (parseInt(localStorage.getItem('currentDay')) || 1);
     const langKey = lang === 'C' ? 'c' : 'python';
 
-    fetch('/Logic-forge/questions.json')
+    fetch('questions.json')
         .then(res => {
             if (!res.ok) throw new Error('Failed to load questions.json: ' + res.status);
             return res.json();
